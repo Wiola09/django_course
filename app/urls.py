87f217476d121f10d5a758d4bf56f6e2c9ab32b0
Miroslav Zeljković <miroslavzeljkovic@gmail.com>
2024-10-25 22:57:04 +0200

    Class-Based Views CRUD

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from app.views import home, kreiraj_tekst
+from app.views import CreateArticleView, ArticleListView, ArticleUpdateView, ArticleDeleteView
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("postovi/kreiraj/", kreiraj_tekst, name="kreiraj_tekst")
+    path("", ArticleListView.as_view(), name="home"),
+    path("kreiraj/", CreateArticleView.as_view(), name="kreiraj_tekst"),
+    path("<int:pk>/izmeni/", ArticleUpdateView.as_view(), name="izmeni_tekst"),
+    path("<int:pk>/obrisi/", ArticleDeleteView.as_view(), name="obrisi_tekst"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,34 +1,35 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 
 from django.http import HttpResponse
 from app.models import Article
-from app.forms import CreateAticleForm
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 # Create your views here.
 
-def home(request):
-    articles = Article.objects.all()
-    return render(request, "app/home.html", {"articles":articles})
-    return HttpResponse("Zdravo Svete Lepi")
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
+
+class CreateArticleView(CreateView):
+    template_name = "app/kreiraj_tekst.html"
+    model = Article
+    fields = ("title", "status", "content", "word_count", "twitter_post")
+    success_url = reverse_lazy("home")
 
 
-def kreiraj_tekst(request):
-    if request.method == "POST":
-        # podaci su predloženi
-        form = CreateAticleForm(request.POST)
-        if form.is_valid():
-            form_data = form.cleaned_data
-            novi_tekst = Article(
-                title=form_data["title"],
-                status=form_data["status"],
-                content=form_data["content"],
-                word_count=form_data["word_count"],
-                twitter_post=form_data["twitter_post"]
-            )
-            novi_tekst.save()
-            return redirect("home")
-    else:
-        form = CreateAticleForm(    
-        )
-    return render(request, "app/kreiraj_tekst.html", {"form":form})
+class ArticleUpdateView(UpdateView):
+    template_name = "app/izmeni_tekst.html"
+    model = Article
+    fields = ("title", "status", "content", "word_count", "twitter_post")
+    success_url = reverse_lazy("home")
+    context_object_name = "articles"
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = "app/obrisi_tekst.html"
+    model = Article
+    success_url = reverse_lazy("home")
+    context_object_name = "articles"
