2a3cfa3c7785583d16ff9bafa0703747ccbfa29f
Miroslav Zeljković <miroslavzeljkovic@gmail.com>
2024-10-25 13:19:11 +0200

    env update

--- djangocourse/settings.py
+++ djangocourse/settings.py
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from . import get_secret
-SECRET_KEY = get_secret('SECRET_KEY')
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [get_secret("HOST")]
+ALLOWED_HOSTS = [os.environ.get("HOST")]
 
 
 # Application definition
