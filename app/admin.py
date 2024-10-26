3e5395365881271a009c27b533d6d57563e20ee6
Miroslav Zeljković <miroslavzeljkovic@gmail.com>
2024-10-26 17:03:08 +0200

    Django admin panel fields

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,14 @@ from app.models import UserProfile, Article
 
 # Register your models here.
 
+class ArticleAdmin (admin.ModelAdmin): 
+    list_display = ("id","title", "word_count", "status", "created_at", "updated_at") 
+    list_filter = ("status",) 
+    search_fields = ("title", "content") 
+    date_hierarchy = "created_at" 
+    ordering = ("created_at",) 
+    readonly_fields = ("word_count", "created_at", "updated_at")
+
 # Da bise moglo videti na admin stranici
 admin.site.register(UserProfile)  
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
